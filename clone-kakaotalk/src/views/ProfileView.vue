e9341a9574e5c4fa6de808d85064e8e8c1a73ad2
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2025-10-29 18:48:01 +0900

    gnb 추가 [ 추후 작업을 위한 커밋 ]

--- clone-kakaotalk/src/views/ProfileView.vue
+++ clone-kakaotalk/src/views/ProfileView.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="view-container view-profile">
-        <div class="view-profile-header">
+        <div class="gnb">
+            <div>
+                <div class="page-title">친구</div>
+                <KSelect type="text">채팅</KSelect> <!-- 클릭 시 옵션 노출-->
+                <KButton type="text">오픈채팅</KButton> <!-- 클릭 시 오픈 채팅 리스트로 이동-->
+            </div>
+            <div class="btn-wrap">
+                <KButton type="icon" icon="ic-search"></KButton>
+                <KButton type="icon" icon="ic-add-friend"></KButton>
+            </div>
         </div>
         <div class="view-profile-body">
             <ProfileCard :profileImage="me.src" :userName="me.name" :profileType="me.type"
@@ -48,11 +57,15 @@
 </template>
 <script>
 import ProfileCard from '@/components/profile/ProfileCard.vue'
+import KButton from '@/components/ui/Button.vue'
+import KSelect from '@/components/ui/Select.vue'
 
 export default {
     name: 'ProfileView',
     components: {
-        ProfileCard
+        ProfileCard,
+        KButton,
+        KSelect
     },
     data() {
         return {
