400fb1e62faa69b6a19734fe6850bb3e3dd497df
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2025-11-28 10:39:11 +0900

    프로필 편집 모달의 제목 변경 및 아바타 컴포넌트 추가, 스타일 개선

--- clone-kakaotalk/src/components/modal/ProfileEditModal.vue
+++ clone-kakaotalk/src/components/modal/ProfileEditModal.vue
@@ -3,14 +3,17 @@
         <div class="modal setBackgroundModal" @click.self="closeModal">
             <div class="modal-content">
                 <div class="modal-header">
-                    <p class="title">배경화면 선택</p>
+                    <p class="title">기본프로필 편집</p>
                 </div>
                 <div class="modal-body">
-                    <KInput type="text" placeholder="이름" value="홍길동" :maxlength="20"/>
-                    <KInput type="text" placeholder="상태메시지 입력" value="상태메시지를 입력해주세요." :maxlength="20"/>
+                    <div class="profile-wrap">
+                        <KAvatar :src="profileImage" :alt="`${userName}의 프로필 이미지입니다.`" :size="90" />
+                        <KInput type="text" placeholder="이름" value="홍길동" :maxlength="20"/>
+                        <KInput type="text" placeholder="상태메시지 입력" value="상태메시지를 입력해주세요." :maxlength="20"/>
+                    </div>
                 </div>
                 <div class="modal-footer">
-                    <KButton color="primary">적용</KButton>
+                    <KButton color="primary">확인</KButton>
                 </div>
             </div>
         </div>
@@ -20,12 +23,13 @@
 <script>
 import KButton from '@/components/ui/Button.vue'
 import KInput from '@/components/ui/Input.vue'
-
+import KAvatar from '@/components/ui/Avatar.vue'
 export default {
     name: 'ProfileEditModal',
     components: {
         KButton,
-        KInput
+        KInput,
+        KAvatar
     },
     emits: ['close'],
     methods: {
@@ -35,3 +39,20 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.profile-wrap {
+    height: 50dvh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    margin-top: 10px;
+    .k-avatar{
+        margin-bottom: 20px;
+    }
+    .k-input{
+        width: 100%;
+    }
+}
+</style>
